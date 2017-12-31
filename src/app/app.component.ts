3ca78094d2a9e1760ed673e692dddbbbdc6f305c
obiols <albertobiolsm@gmail.com>
2017-12-31 17:32:20 +0100

    step_06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,25 +11,10 @@ import { HttpErrorResponse } from '@angular/common/http/src/response';
 export class AppComponent {
 
   people$;
-  message;
 
   constructor(private peopleService: PeopleService) {}
 
   fetchPeople() {
-    this.peopleService.fetchPeople()
-      .subscribe(
-        (data) => {
-          this.message = null;
-          this.people$ = data;
-        },
-        (err: HttpErrorResponse) => {
-          if (err instanceof Error) {
-            // client-side error
-            this.message = `An error occured ${err.error.message}`;
-          } else {
-            this.message = `Backend returned error code ${err.status}, body was: ${err.message}`;
-          }
-        }
-    );
+    this.people$ = this.peopleService.fetchPeople();
   }
 }
--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -9,19 +9,13 @@ import 'rxjs/add/operator/delay';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/observable/of';
 
-export interface Person {
-  name: string;
-}
-
 @Injectable()
 export class PeopleService {
 
   constructor(private http: HttpClient) { }
 
   fetchPeople(): Observable<Object> {
-    return this.http.get<Person>('https://reqres.in/api/users/2', {
-      headers: new HttpHeaders().set('app-language', 'it')
-    });
+    return this.http.get('/assets/data/people.txt', { responseType: 'text'});
   }
 
 }
