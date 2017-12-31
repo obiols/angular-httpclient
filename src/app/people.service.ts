fbaf5eff1ad28475c37d3c62aee55b5bd5cb4758
obiols <albertobiolsm@gmail.com>
2017-12-31 16:41:01 +0100

    step_04

--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
-
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/retry';
+import 'rxjs/add/operator/retryWhen';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/of';
 
 export interface Person {
   name: string;
@@ -13,15 +19,19 @@ export class PeopleService {
   constructor(private http: HttpClient) { }
 
   fetchPeople(): Observable<Object> {
-    /*
-    const params = new HttpParams()
-      .set('id', '2');
-
-    return this.http.get<Person>('https://reqres.in/api/users/', {
-      params: params
-    });
-    */
-    return this.http.get<Person>('https://reqres.in/api/users/23');
+    return this.http.get<Person>('https://reqres.in/api/users/23')
+      .retryWhen(err => {
+        let retries = 3;
+        return err
+          .delay(1000)
+          .flatMap((err) => {
+            if (retries-- > 0) {
+              return Observable.of(err);
+            } else {
+              return Observable.throw(err);
+            }
+          });
+      });
   }
 
 }
